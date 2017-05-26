609b7fa67bc482d2ae6a20b610e1b426d30da89c
NatalyAlvarez8655176 <naty7sex@gmail.com>
2017-05-26 14:32:42 -0400

    admin

---
+++ client/soporteModule/admicurso.js
@@ -0,0 +1,10 @@
+Template.admicurso.events({
+	'click #btnnombreC': function (e) {
+		e.preventDefault();
+		var nombre = document.getElementById("nombreC").value;
+
+		Meteor.call('agregarMateria', nombre, Accounts.userId());
+
+	}
+	
+});
